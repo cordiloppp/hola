10b03df5ad08ae019d3211136b18d794c91e8b73
cordilop <jonathanmolinero12@gmail.com>
2024-08-30 10:48:13 -0700

    final

--- nextjs-dashboard/app/ui/button.tsx
+++ nextjs-dashboard/app/ui/button.tsx
@@ -1,5 +1,4 @@
 import clsx from 'clsx';
-import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
